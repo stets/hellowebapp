b900ba6bf8b4975bd2a7c438e57de5cfbbe37ac7
stets <stetsblake@gmail.com>
2017-10-15 11:44:43 -0400

    Added templates for about and contact pages. Played around with template tags on index

--- collection/views.py
+++ collection/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-        #this is your brand damn new view
-        return render(request, 'index.html')
+
+     #defining a variable
+     number = 6
+     thing = "Thing name"   
+     #passing the var into our view!
+     return render(request, 'index.html', {
+         'number': number,
+         'thing': thing,
+    })
