84c83029c3fa597c6cd63de5bec347c7c13bca53
stets <stetsblake@gmail.com>
2017-10-17 17:12:55 -0400

    Added authentication and ability to reset passwords via django-registration plugin

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -1,7 +1,15 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
+from django.contrib.auth.views import (
+        password_reset,
+        password_reset_done, 
+        password_reset_confirm, 
+        password_reset_complete
+)
+
+
 
 urlpatterns = [
     url(r'^$', views.index, name='home'), 
@@ -17,5 +25,34 @@ urlpatterns = [
     url(r'^things/(?P<slug>[-\w]+)/edit/$',
         views.edit_thing, 
         name='edit_thing'),
+#
+# Begin massive list of password reset URLS...
+#
+   url(r'^accounts/password/reset/$',
+       password_reset,
+       {'template_name':
+           'registration/password_reset_form.html'},
+            name="password_reset"),
+   url(r'^accounts/password/reset/done/$', 
+        password_reset_done, 
+        {'template_name':
+        'registration/password_reset_done.html'},
+        name="password_reset_done"),
+   url(r'^accounts/password/reset/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/?',
+       password_reset_confirm, 
+       {'template_name':
+           'registration/password_reset_confirm.html'},
+            name="password_reset_confirm"),
+   url(r'^accounts/password/done/$',
+       password_reset_complete, 
+       {'template_name':
+           'registration/password_reset_complete.html'},
+        name="password_reset_complete"),
+   url(r'^accounts/', 
+       include('registration.backends.simple.urls')),
+   url(r'^admin/', admin.site.urls),
+    
+   url(r'^accounts/', 
+        include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
 ]
