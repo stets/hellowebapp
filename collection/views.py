d0dc6f9b3332b174529014e3bf2620c3ee4ef57f
stets <stetsblake@gmail.com>
2017-10-17 13:32:07 -0400

    Removed ignored files and added an edit form to edit posts!

--- collection/views.py
+++ collection/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from collection.models import Thing
+from django.shortcuts import render, redirect
 
+from collection.forms import ThingForm
+from collection.models import Thing
 
 def index(request):
 
@@ -22,3 +23,31 @@ def thing_detail(request, slug):
     return render(request, 'things/thing_detail.html', {
         'thing': thing,
         })
+
+def edit_thing(request, slug):
+    # grab the object
+    thing = Thing.objects.get(slug=slug)
+    # set the form we are using
+    form_class = ThingForm
+
+    #if coming to this view from a submitted form :
+    if request.method == 'POST':
+        # grab data from form and apply to the form
+        form = form_class(data=request.POST, instance=thing)
+        if form.is_valid():
+            # save new data
+            form.save()
+            return redirect('thing_detail', slug=thing.slug)
+
+    # otherwise, just create the form
+    else:
+        form = form_class(instance=thing)
+
+
+    # and render the template
+    return render(request, 'things/edit_thing.html', {
+            'thing': thing, 
+            'form' : form, 
+            })
+
+
